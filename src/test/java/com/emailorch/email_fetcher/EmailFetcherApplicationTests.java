d4cf2b43958d38809d734ac0f6e69611299a5a69
mostafa3ssa <mostafamohamedessa10@gmail.com>
2026-03-24 23:48:35 +0200

    initial commit

---
+++ src/test/java/com/emailorch/email_fetcher/EmailFetcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emailorch.email_fetcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailFetcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
